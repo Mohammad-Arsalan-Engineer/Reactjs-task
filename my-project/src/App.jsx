f27c2e6646b09bbfffbd238fef6cebbf37f80354
Mohammad-Arsalan-Engineer <155758752+Mohammad-Arsalan-Engineer@users.noreply.github.com>
2024-02-05 05:46:38 -0800

    Initial commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,88 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react';
+import './App.css';
 
 function App() {
-  const [color, setColor] = useState("olive")
- 
+  const [length, setLength] = useState(8);
+  const [number, setAllowedNumber] = useState(false);
+  const [character, setCharacter] = useState(false);
+  const [Password, setPassword] = useState("");
+
+
+  /////////////useRef hook////////////
+
+  const passwordRef = useRef()
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+
+    if (number) str += "0123456789";
+    if (character) str += "{}@#$%^&*()!~";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, number, character, setPassword]);
+
+  const copyPasswordTo = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(Password)
+  } , [Password])
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, number, character, passwordGenerator]);
 
   return (
-   <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
-   <div className='fixed flex justify-center flex-wrap inset-x-0 bottom-12 px-2'>
-    <div className='flex justify-center flex-wrap bg-white px-3 shadow-lg py-2 gap-3 rounded-3xl'>
-    <button onClick={() => setColor("red") } className='outline-none px-3 rounded-full text-white shadow-lg py-1' style={{background: "red"}}>Red</button>
-    <button onClick={() => setColor("green") }  className='outline-none px-3 rounded-full text-white shadow-lg py-1' style={{background: "green"}}>Green</button>
-    <button onClick={() => setColor("blue") }  className='outline-none px-3 rounded-full text-white shadow-lg py-1' style={{background: "blue"}}>Blue</button>
-    <button onClick={() => setColor("purple") }  className='outline-none px-3 rounded-full text-white shadow-lg py-1' style={{background: "purple"}}>Purple</button>
-    </div>
-    
-   </div>
-   </div>
-  )
+    <>
+      <div className='w-full max-w-md max-auto shadow-md rounded-lg px-4 py-4  my-8 text-orange-500 bg-gray-500'>
+        <h1 className='text-center text-white my-4'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type='text'
+            value={Password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='password'
+            readOnly
+            ref={passwordRef}
+          />
+          <button onClick={copyPasswordTo} className='px-4 rounded-none outline-none py-4 ml-3 bg-black'>Copy</button>
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type='range'
+              min={8}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>length: {length} </label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type='checkbox'
+              id='numberInput'
+              onChange={() => { setAllowedNumber((prev) => !prev) }}
+            />
+            <label htmlFor='numberInput'>Number</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type='checkbox'
+              id='characterInput'
+              onChange={() => { setCharacter((prev) => !prev) }}
+            />
+            <label htmlFor='characterInput'>Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
